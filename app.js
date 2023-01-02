a52ab393a312444fa87902a7fb702abd70a8347a
meesejordan <meesejordan@gmail.com>
2023-01-02 15:32:58 -0700

    Added barebone express server in app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+require("express-async-errors");
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Navajo Dictionary API");
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        app.listen(
+            port,
+            console.log(`Server is listening on port ${port}....`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
