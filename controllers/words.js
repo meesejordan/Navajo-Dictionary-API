314361afbfbdd2820d292b8534e075b83a2975b8
meesejordan <meesejordan@gmail.com>
2023-01-02 19:48:02 -0700

    Added ability to select which fields to return in getAllWords controller

--- controllers/words.js
+++ controllers/words.js
@@ -3,36 +3,37 @@ const Word = require("../models/word");
 const getAllWordsStatic = async (req, res) => {
     const queryObject = {};
     // queryObject.wordDefinitions = { $regex: "it", $options: "i" };
-    const words = await Word.find(queryObject).sort("word");
+    const words = await Word.find(queryObject).sort("word").select("");
 
     res.status(200).json({ words, numWords: words.length });
 };
 
 const getAllWords = async (req, res) => {
     console.log(req.query);
-    const { search, sort } = req.query; //, fields } = req.query;
-    test = req.query.sort;
+    const { search, fields } = req.query;
     const queryObject = {};
+    let sortList = "";
+    let fieldsList = "";
 
-    // check ifsearch parameter is pass, then search wordDefinitions for the search value
+    // check if search parameter is passed, then search wordDefinitions for the search value
     if (search) {
         queryObject.wordDefinitions = { $regex: search, $options: "i" };
     }
 
-    let result = await Word.find(queryObject);
     // sort on given values or sort on creation date by default
-    if (test) {
+    if (req.query.sort) {
         // get list of values to sort on
-        const sortList = test.split(",").join(" ");
-        console.log(sortList);
-        result = await result.sort(sortList);
+        sortList = req.query.sort.split(",").join(" ");
+        // console.log(sortList);
     }
 
-    // // Select current fields, like SQL select
-    // if (fields) {
-    //     const fieldsList = fields.split(",").join(" ");
-    //     result = result.select(fieldsList);
-    // }
+    // Select current fields, like SQL select
+    if (fields) {
+        fieldsList = fields.split(",").join(" ");
+    }
+
+    // Finally, search words based on search query, get words sorted on given sort value and finally return the fields of the given field values
+    let result = await Word.find(queryObject).sort(sortList).select(fieldsList);
     res.status(200).json({ result, numWords: result.length });
 };
 
