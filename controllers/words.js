965de3c4f996f645075e08b875e443b4ff3aac48
meesejordan <meesejordan@gmail.com>
2023-01-02 17:50:16 -0700

    Added word selection in the controller based on name, sort, fields

--- controllers/words.js
+++ controllers/words.js
@@ -1,5 +1,31 @@
 const Word = require("../models/word");
 
-const getAllWords = async (req, res) => {
+const getAllWordsStatic = async (req, res) => {
     const products = await Product.find({});
+};
+
+const getAllProducts = async (req, res) => {
+    const { name, sort, fields } = req.body;
+    const queryObject = {};
+
+    // check if name is provided
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+
+    let result = Word.find(queryObject);
+    // sort on given values or sort on creation date by default
+    if (sort) {
+        // get list of values to sort on
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+    } else {
+        result = result.sort("createdAt");
+    }
+
+    // Select current fields, like SQL select
+    if (fields) {
+        const fieldsList = fields.split(",").join(" ");
+        result = result.select(fieldsLists);
+    }
 };
