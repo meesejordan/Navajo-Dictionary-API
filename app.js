5fddde1804997cbaf2f22ea05397414ffe4be2de
meesejordan <meesejordan@gmail.com>
2023-01-02 23:17:11 -0700

    Added extra protecter and limiter middleware for api

--- app.js
+++ app.js
@@ -10,8 +10,28 @@ const connectDB = require("./db/connectDB");
 // import middleware
 const notFoundMiddleware = require("./middleware/not-found");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+// app.set("trust proxy", 1) //using reverse proxies like Heroku
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, //15 minute
+        max: 100, // 100 requires per windowMs
+    })
+);
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // import routers
 const wordsRouter = require("./routes/words");
+
 app.get("/", (req, res) => {
     res.send("Navajo Dictionary API");
 });
