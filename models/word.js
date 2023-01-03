6743ca77d94a7eda946a9c1be4d59963fab8999c
meesejordan <meesejordan@gmail.com>
2023-01-02 17:32:20 -0700

    Created words.js to control db words

--- models/word.js
+++ models/word.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
+// word => Navajo word ex."Yá'át'ééh"
+// wordDefition => array of definitions ex. ["Hello", "It is good", "Greetings"]
+// wordAudio => string link with spoken word ex. "https://dl.dropbox.com/s/z3gm2v9ezt4vdk1/commonNavajo-1-0",
+// examples => array of example use of the word. ex. [// "Yá'át'ééh dóó ahéehee:Hello and thank you",// "Yá'át'ééh abini:Good morning",// ]
+// examplesAudio => array of urls of the given examples. ex: [ //"https://dl.dropbox.com/s/z3gm2v9ezt4vdk1/commonNavajo-1-0", //"https://dl.dropbox.com/s/j0g7iy2gmjig1k8/commonNavajo-1-1", // ],
+
+// 'examples' indices map to 'audioExamples'. IFF audioExamples exist
 const wordSchema = new mongoose.Schema(
     {
         word: {
             type: String,
             required: [true, "Navajo word must be provided"],
+            unique: true,
         },
         wordDefinitions: {
             type: [String],
@@ -13,11 +21,13 @@ const wordSchema = new mongoose.Schema(
                 "One Definition for the given Navajo word must be provided",
             ],
         },
-        wordAudio: String,
+        wordAudio: {
+            type: String,
+        },
         examples: {
             type: [String],
         },
-        exampleAudio: [String],
+        examplesAudio: [String],
     },
     { timestamps: true }
 );
